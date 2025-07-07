955dafb5c4bd066a97677092e35f315c4f9cb337
Wesley Gurgel <wesleygurgel27@gmail.com>
2025-07-07 17:16:49 -0300

    fix: fiscall

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import About from './pages/About'
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </Layout>
   )
